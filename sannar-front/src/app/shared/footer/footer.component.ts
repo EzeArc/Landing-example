ee108484c8b25475468ab5f0de5778605ba5ee73
Ezequiel <ezequielarce_1996@hotmail.com>
2024-06-04 18:39:32 -0300

    fix: footer redirect from logo & header line-height (about/contact-page)

--- sannar-front/src/app/shared/footer/footer.component.ts
+++ sannar-front/src/app/shared/footer/footer.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { SannarLogoComponent } from '../sannar-logo/sannar-logo.component';
+import { RouterLink } from '@angular/router';
 
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [SannarLogoComponent,],
+  imports: [SannarLogoComponent,RouterLink],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
