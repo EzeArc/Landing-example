af878b57f9310a7d47ac9d770ed87f70273bd1c2
Ezequiel <ezequielarce_1996@hotmail.com>
2024-06-06 15:04:30 -0300

    fix: img with & height attributes. Move hero-img to shared

--- sannar-front/src/app/views/about-page/about-page.component.ts
+++ sannar-front/src/app/views/about-page/about-page.component.ts
@@ -1,18 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AboutCardsComponent } from '../../components/about-cards/about-cards.component';
-import { HeroImgComponent } from '../../components/hero-img/hero-img.component';
 import { WeedLoaderComponent } from '../../shared/weed-loader/weed-loader.component';
 import { GrowKitComponent } from '../../components/grow-kit/grow-kit.component';
 import { AboutUsInfoComponent } from '../../components/about-us-info/about-us-info.component';
 import * as data from '../../../assets/data/content.json'
+import { HeroImgComponent } from "../../shared/hero-img/hero-img.component";
 
 @Component({
-  selector: 'app-about-page',
-  standalone: true,
-  imports: [RouterOutlet, AboutCardsComponent, HeroImgComponent, WeedLoaderComponent, GrowKitComponent, AboutUsInfoComponent],
-  templateUrl: './about-page.component.html',
-  styleUrl: './about-page.component.css'
+    selector: 'app-about-page',
+    standalone: true,
+    templateUrl: './about-page.component.html',
+    styleUrl: './about-page.component.css',
+    imports: [RouterOutlet, AboutCardsComponent, HeroImgComponent, WeedLoaderComponent, GrowKitComponent, AboutUsInfoComponent]
 })
 export class AboutPageComponent {
 
--- sannar-front/src/app/views/contact-page/contact-page.component.ts
+++ sannar-front/src/app/views/contact-page/contact-page.component.ts
@@ -1,15 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FaqSectionComponent } from '../../components/faq-section/faq-section.component';
-import { HeroImgComponent } from '../../components/hero-img/hero-img.component';
 import { ContactFormComponent } from '../../components/contact-form/contact-form.component';
 import * as data from '../../../assets/data/content.json'
+import { HeroImgComponent } from "../../shared/hero-img/hero-img.component";
 @Component({
-  selector: 'app-contact-page',
-  standalone: true,
-  imports: [RouterOutlet, HeroImgComponent,ContactFormComponent,FaqSectionComponent],
-  templateUrl: './contact-page.component.html',
-  styleUrl: './contact-page.component.css'
+    selector: 'app-contact-page',
+    standalone: true,
+    templateUrl: './contact-page.component.html',
+    styleUrl: './contact-page.component.css',
+    imports: [RouterOutlet, ContactFormComponent, FaqSectionComponent, HeroImgComponent]
 })
 export default class ContactPageComponent {
   title= data.CONTACT_PAGE.HERO_TITLE;
