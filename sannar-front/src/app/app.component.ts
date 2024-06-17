9b251214f6c33487e22d0df6d3a8b7ccf2653c0f
Ezequiel <ezequielarce_1996@hotmail.com>
2024-06-17 17:27:32 -0300

    fix: update @media from article.page

--- sannar-front/src/app/app.component.ts
+++ sannar-front/src/app/app.component.ts
@@ -2,8 +2,6 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './shared/navbar/navbar.component';
 import { FooterComponent } from './shared/footer/footer.component';
-import '@fontsource-variable/onest';
-
 
 @Component({
   selector: 'app-root',
